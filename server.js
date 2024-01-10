49c03ce36d70de18ccc56f52c9b06864e4755aa9
J Mac <jbatcheller123@gmail.com>
2024-01-09 21:09:43 -0700

    I added the uuid, it is not quite working yet, but we are getting there. I moved my wild card request below my get request to fix the issue I was having where the notes were not displaying on the front end

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-
+const uuid = require('./public/helpers/uuid')
+const notes = require('./db/db.json');
 const app = express();
 const port = 3001;
 
 //middleware for static assets 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static('public'));
 
 // get request to return the notes.html file 
@@ -14,6 +16,8 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => res.status(201).json(notes));
 
 // get request to return the index.html file
 
@@ -23,10 +27,9 @@ app.get('*', (req, res) => {
 
 // get and post resquests to read and save the added notes
 
-app.get('/api/notes', (req, res) => {
 
-    res.json(`${req.method}`)
-});
+
+   
 
 app.post('/api/notes', (req, res) => {
 
@@ -40,46 +43,71 @@ app.post('/api/notes', (req, res) => {
 
         const savedNotes = {
             title,
-            text
+            text, 
+            id: uuid(),
         };
 
         // get the existing notes from the db.json file
 
-        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-            if (err) {
-                console.error(err);
-            } else {
-
-                const parsedNotes = JSON.parse(data);
-
-                parsedNotes.push(savedNotes);
-
-
-                // write the updated notes from the front end
-
-                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeErr) =>
-                    writeErr
-                        ? console.error(writeErr)
-                        : console.info('Successfully updated notes')
-                );
-            }
-        });
-
-        const finalNote = {
-            status: 'Success!',
-            body: savedNotes,
-        }
-
-        console.log(finalNote); 
-        res.status(201).json(finalNote); 
+       readAndAppend(savedNotes, './db/db.json'); 
+       res.json('Note Successfully Added!');  
     } else {
         res.status(500).json('Error in adding note');
     }
 
-    });
+});
 
 
-    app.listen(port, () => {
-        console.log(`App listening on http://localhost:${port}`);
-    });
 
+
+app.listen(port, () => {
+    console.log(`App listening on http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const finalNote = {
+//     status: 'Success!',
+//     body: savedNotes,
+// }
+// console.log(finalNote);
+//         res.status(201).json(finalNote);
+
+
+
+// fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     } else {
+
+//         const parsedNotes = JSON.parse(data);
+
+//         parsedNotes.push(savedNotes);
+
+
+//         // write the updated notes from the front end
+
+//         fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeError) =>
+//             writeError
+//                 ? console.error(writeError)
+//                 : console.info('Successfully updated notes')
+//         );
+//     }
+// });

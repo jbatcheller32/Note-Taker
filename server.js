829003517ce49b3bf713f48f82a6a3a3e16ce6ee
J Mac <jbatcheller123@gmail.com>
2024-01-11 16:30:16 -0700

    I have the app working, the only thing is the notes only show up after I restart the server. Still trying to figure that out

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3001;
 
 //middleware for static assets 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // get request to return the notes.html file 
@@ -17,19 +17,14 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+
+// GET request for the /api/notes route
+
 app.get('/api/notes', (req, res) => res.status(201).json(notes));
 
-// get request to return the index.html file
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
-});
+// POST resquest to read and save the added notes then send to the client
 
-// get and post resquests to read and save the added notes
-
-
-
-   
 
 app.post('/api/notes', (req, res) => {
 
@@ -43,14 +38,40 @@ app.post('/api/notes', (req, res) => {
 
         const savedNotes = {
             title,
-            text, 
+            text,
             id: uuid(),
         };
 
         // get the existing notes from the db.json file
 
-       readAndAppend(savedNotes, './db/db.json'); 
-       res.json('Note Successfully Added!');  
+        fs.readFile('./db/db.json', 'utf-8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+
+                const parsedNotes = JSON.parse(data);
+
+                parsedNotes.push(savedNotes);
+
+
+                // write the updated notes from the front end
+
+                fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeError) =>
+                    writeError
+                        ? console.error(writeError)
+                        : console.info('Successfully updated notes')
+                );
+            }
+        });
+
+
+        const finalNote = {
+            status: 'Success!',
+            body: savedNotes,
+        }
+        console.log(finalNote);
+        res.status(201).json(finalNote);
+
     } else {
         res.status(500).json('Error in adding note');
     }
@@ -60,54 +81,16 @@ app.post('/api/notes', (req, res) => {
 
 
 
+// get request to return the index.html file
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
+// server spin up 
+
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const finalNote = {
-//     status: 'Success!',
-//     body: savedNotes,
-// }
-// console.log(finalNote);
-//         res.status(201).json(finalNote);
-
-
-
-// fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-//     if (err) {
-//         console.error(err);
-//     } else {
-
-//         const parsedNotes = JSON.parse(data);
-
-//         parsedNotes.push(savedNotes);
-
-
-//         // write the updated notes from the front end
-
-//         fs.writeFile('./db/db.json', JSON.stringify(parsedNotes, null, 4), (writeError) =>
-//             writeError
-//                 ? console.error(writeError)
-//                 : console.info('Successfully updated notes')
-//         );
-//     }
-// });
